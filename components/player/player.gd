3250903dd2f099d9134dc100193ea354783921d9
cora_lyn <caroroemer@gmx.de>
2026-04-14 14:17:46 +0200

    Added simple player movement; changed stretch mode in project settings

--- components/player/player.gd
+++ components/player/player.gd
@@ -1,2 +1,9 @@
 class_name Player
 extends CharacterBody2D
+
+const SPEED := 100
+
+func _process(delta: float) -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+	move_and_slide()
