17fb6466978d5507207c375b97234f2947e4c343
cora_lyn <caroroemer@gmx.de>
2026-04-18 19:45:32 +0200

    Added Fog, fog intensity shrinks if fear also shrinks

---
+++ Fog.gd
@@ -0,0 +1,10 @@
+extends ParallaxBackground
+
+@onready var color_rect: ColorRect = $ParallaxLayer/ColorRect
+
+func _ready() -> void:
+	Signals.item_collected.connect(reduce_fog)
+
+func reduce_fog(_item: Item):
+	var modulate_val = (GameManager.current_fear / GameManager.init_fear)
+	color_rect.modulate = Color(1,1,1,modulate_val)
