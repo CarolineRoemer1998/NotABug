2c0abff172ce1e7ba96ba6a0019e85895286897a
cora_lyn <caroroemer@gmx.de>
2026-04-18 19:00:49 +0200

    Implemented animated item assets

--- components/items/ItemData.gd
+++ components/items/ItemData.gd
@@ -4,5 +4,5 @@ extends Resource
 @export var item_name : String
 @export var type : Item.TYPE
 @export var fear_reduction : float # Wert, um den Fear-Meter gesenkt wird
-@export var item_texture : Texture2D
+@export var item_texture : SpriteFrames
 @export var wearable_texture : Texture2D
--- components/items/item.gd
+++ components/items/item.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 enum TYPE { Hat, MeleeWeapon, RangedWeapon, Shoes, Mouth, Top, Bottom } # nicht final
 
-@onready var sprite_2d_as_collectable_item: Sprite2D = $Visuals/Sprite2D_AsCollectableItem
+@onready var sprite_2d_as_collectable_item: AnimatedSprite2D = $Visuals/Sprite2D_AsCollectableItem
 @onready var sprite_2d_as_worn_by_enemy: Sprite2D = $Visuals/Sprite2D_AsWornByEnemy
 
 const ITEM : PackedScene = preload("res://components/items/item.tscn")
@@ -18,6 +18,7 @@ static func get_new_item(_item_data: ItemData):
 func _ready() -> void:
 	Signals.item_collected.connect(handle_item_collected)
 	set_data()
+	sprite_2d_as_collectable_item.play("floating")
 
 func handle_item_collected(_item: Item):
 	if _item == self:
@@ -25,4 +26,4 @@ func handle_item_collected(_item: Item):
 
 func set_data():
 	name = item_data.item_name
-	sprite_2d_as_collectable_item.texture = item_data.item_texture
+	sprite_2d_as_collectable_item.sprite_frames = item_data.item_texture
