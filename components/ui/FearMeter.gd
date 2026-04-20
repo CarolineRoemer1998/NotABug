4c8133020d1b535e410a0472b4d769701c10c840
Caro <74654064+CarolineRoemer1998@users.noreply.github.com>
2026-04-20 15:20:46 +0200

    final commit

--- components/ui/FearMeter.gd
+++ components/ui/FearMeter.gd
@@ -6,31 +6,54 @@ extends ProgressBar
 
 var shake_power := 0.006
 var shake_color_rate := 0.006
+var base_fill_color: Color
 
 func _ready() -> void:
 	Signals.item_collected.connect(update_progress_bar)
+
+	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
+	var fill_style_copy := fill_style.duplicate() as StyleBoxFlat
+	add_theme_stylebox_override("fill", fill_style_copy)
+	base_fill_color = fill_style_copy.bg_color + Color(-0.05, 0.1, 0.03)
+
+	if glitchoverlay.material != null:
+		glitchoverlay.material = glitchoverlay.material.duplicate()
+
+	reset_runtime_state()
 	reset_glitch_shader()
 
-func update_progress_bar(_item: Item):
+func reset_runtime_state() -> void:
+	shake_power = 0.006
+	shake_color_rate = 0.006
+
+	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
+	fill_style.bg_color = base_fill_color
+
+func update_progress_bar(_item: Item) -> void:
 	value = GameManager.current_fear
+
 	var mat := glitchoverlay.material as ShaderMaterial
-	mat.set_shader_parameter("shake_power", 0.05)
-	mat.set_shader_parameter("shake_color_rate", 0.03)
+	if mat != null:
+		mat.set_shader_parameter("shake_power", 0.05)
+		mat.set_shader_parameter("shake_color_rate", 0.03)
+
 	shake_power -= 0.002
 	shake_color_rate -= 0.002
 	timer_glitch.start()
-	
-
 
 func _on_timer_glitch_timeout() -> void:
 	reset_glitch_shader()
 
-func reset_glitch_shader():
+func reset_glitch_shader() -> void:
 	var mat := glitchoverlay.material as ShaderMaterial
+	if mat == null:
+		return
+
 	mat.set_shader_parameter("shake_power", shake_power)
 	mat.set_shader_parameter("shake_rate", 1.0)
 	mat.set_shader_parameter("shake_speed", 0.001)
 	mat.set_shader_parameter("shake_block_size", 0.001)
 	mat.set_shader_parameter("shake_color_rate", shake_color_rate)
+
 	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
-	fill_style.bg_color += Color(-0.05, 0.1, 0.03)
+	fill_style.bg_color = base_fill_color
--- components/ui/health_meter.gd
+++ components/ui/health_meter.gd
@@ -6,34 +6,51 @@ extends ProgressBar
 
 var shake_power := 0.002
 var shake_color_rate := 0.002
+var base_fill_color: Color
 
 func _ready() -> void:
-	print("_ready")
+	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
+	var fill_style_copy := fill_style.duplicate() as StyleBoxFlat
+	add_theme_stylebox_override("fill", fill_style_copy)
+	base_fill_color = fill_style_copy.bg_color
+
+	if glitchoverlay.material != null:
+		glitchoverlay.material = glitchoverlay.material.duplicate()
+
+	reset_runtime_state()
 	reset_glitch_shader()
 
+func reset_runtime_state() -> void:
+	shake_power = 0.002
+	shake_color_rate = 0.002
+
+	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
+	fill_style.bg_color = base_fill_color
+
 func _on_timer_glitch_timeout() -> void:
-	print("_on_timer_glitch_timeout")
 	reset_glitch_shader()
-#
-func reset_glitch_shader():
-	print("reset_glitch_shader")
+
+func reset_glitch_shader() -> void:
 	var mat := glitchoverlay.material as ShaderMaterial
+	if mat == null:
+		return
+
 	mat.set_shader_parameter("shake_power", shake_power)
 	mat.set_shader_parameter("shake_rate", 1.0)
 	mat.set_shader_parameter("shake_speed", 0.001)
 	mat.set_shader_parameter("shake_block_size", 0.001)
 	mat.set_shader_parameter("shake_color_rate", shake_color_rate)
 
-
-
-func _on_value_changed(value):
-	print("_on_value_changed")
+func _on_value_changed(value) -> void:
 	var mat := glitchoverlay.material as ShaderMaterial
-	mat.set_shader_parameter("shake_power", 0.005)
-	mat.set_shader_parameter("shake_color_rate", 0.03)
+	if mat != null:
+		mat.set_shader_parameter("shake_power", 0.005)
+		mat.set_shader_parameter("shake_color_rate", 0.03)
+
 	shake_power += 0.003
 	shake_color_rate += 0.002
+
 	var fill_style := get_theme_stylebox("fill") as StyleBoxFlat
-	fill_style.bg_color += Color(0.4, -0.1, -0.1)
-	
+	fill_style.bg_color = fill_style.bg_color + Color(0.4, -0.1, -0.1)
+
 	timer_glitch.start()
